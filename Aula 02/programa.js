9d4c1309507e9a0af65519ddb761a475dcba72a3
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2019-02-22 20:32:34 -0300

    boilerplate

---
+++ Aula 02/programa.js
@@ -0,0 +1,23 @@
+/* Variávei globais */
+
+let canvas;
+
+function getCanvas(){
+    return document.querySelector("canvas");
+}
+
+function main() {
+// 1 - Carregar tela de desenho
+    canvas = getCanvas();
+
+// 2 - Carregar o contexto (API) WebGL
+// 3 - Ler os arquivos de shader
+// 4 - Compilar arquivos de shader
+// 5 - Linkar o programa de shader
+// 6 - Criar dados de parâmetro
+// 7 - Transferir os dados para GPU
+// 8 - Chamar o loop de redesenho
+
+}
+
+window.addEventListener("load", main);
