c4a1ffe39286deb903832afc255127f548c98a12
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2019-02-22 21:18:56 -0300

    read file

--- Aula 02/programa.js
+++ Aula 02/programa.js
@@ -1,17 +1,31 @@
 /* Variávei globais */
 
-let canvas;
+let canvas,
+    gl,
+    vertexShaderSource,
+    fragmentShaderSource;
 
 function getCanvas(){
     return document.querySelector("canvas");
 }
 
-function main() {
+function getGLContext(canvas) {
+    let gl = canvas.getContext("webgl");
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    return gl;
+}
+
+async function main() {
 // 1 - Carregar tela de desenho
     canvas = getCanvas();
 
 // 2 - Carregar o contexto (API) WebGL
+    gl = getGLContext(canvas);
+
 // 3 - Ler os arquivos de shader
+    vertexShaderSource = await fetch("vertex.glsl").then(r => r.text());
+    console.log("VERTEX", vertexShaderSource);
+
 // 4 - Compilar arquivos de shader
 // 5 - Linkar o programa de shader
 // 6 - Criar dados de parâmetro
