73437a9b44317f50e22ef4b374418b1e8fb6d015
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2019-02-22 21:43:11 -0300

    compile

--- Aula 02/programa.js
+++ Aula 02/programa.js
@@ -3,7 +3,9 @@
 let canvas,
     gl,
     vertexShaderSource,
-    fragmentShaderSource;
+    fragmentShaderSource,
+    vertexShader,
+    fragmentShader;
 
 function getCanvas(){
     return document.querySelector("canvas");
@@ -13,6 +15,15 @@ function getGLContext(canvas) {
     let gl = canvas.getContext("webgl");
     gl.viewport(0, 0, canvas.width, canvas.height);
     return gl;
+}
+
+function compileShader(source, type, gl){
+    let shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+        console.error("ERRO NA COMPILAÇÃO", gl.getShaderInfoLog(shader));
+    return shader;
 }
 
 async function main() {
@@ -30,6 +41,8 @@ async function main() {
     console.log("FRAGMENT", fragmentShaderSource);
 
 // 4 - Compilar arquivos de shader
+    vertexShader = compileShader(vertexShaderSource, gl.VERTEX_SHADER, gl);
+    
 // 5 - Linkar o programa de shader
 // 6 - Criar dados de parâmetro
 // 7 - Transferir os dados para GPU
