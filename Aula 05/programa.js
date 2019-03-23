836da81c9078d423e25af9c84181ed73c6048f39
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2019-03-22 21:37:40 -0300

    cubo

--- Aula 05/programa.js
+++ Aula 05/programa.js
@@ -155,7 +155,7 @@ async function main() {
     window.addEventListener("resize", resize);
 
     // 7.2 - VIEW MATRIX UNIFORM
-    eye  = [4, -3, 5];
+    eye  = [4, 3, -5];
     let up = [0, 1, 0];
     let center = [0, 0, 0];
     view = mat4.lookAt([], eye, center, up);
