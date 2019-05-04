a2218bdb2c8d85df8cb39e8caa8e2ed5ffeb6d57
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2019-05-03 21:00:12 -0300

    comentários

--- Aula 07/fragment.glsl
+++ Aula 07/fragment.glsl
@@ -7,10 +7,21 @@ varying vec4 vertexNormal;
 uniform vec3 color;
 
 void main() {
+
+    // POSIÇÃO DA LUZ
     vec4 light = vec4(10.0, 10.0, 10.0, 1.0);
+
+    // VETOR DE INCIDÊNCIA
     vec4 L = normalize(light - vertexPos);
+
+    // NORMAL (RENORMALIZADA)
     vec4 N = normalize(vertexNormal);
+
+    // CÁLCULO DE LAMBERT
     float Lambert = max(dot(L,N), 0.0);
+    
+    // NOVA COR TONALIZADA
     vec3 shade = color * Lambert; 
+
     gl_FragColor = vec4(shade, 1.0);
 }
